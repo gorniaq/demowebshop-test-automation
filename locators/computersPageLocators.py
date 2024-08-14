4419c525d90b3b9446f4f64135b98b24d0a51a60
Anton Kryvtsov <gorniaq@gmail.com>
2024-08-14 23:00:10 +0300

    added Verify that Computers group has 3 sub-groups with correct names

---
+++ locators/computersPageLocators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+class ComputersPageLocators:
+
+    CATEGORY_LIST = (By.CLASS_NAME, "sub-category-grid")
+    CATEGORY_ITEMS = (By.CSS_SELECTOR, ".sub-category-item .title a")
